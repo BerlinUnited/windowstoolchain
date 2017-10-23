3393bbbd90801f2b4f009d1ef2fd58986e2cfc23
Benjamin Schlotter <benji@schlotterfam.de>
2017-10-23 14:20:53 +0200

    added premake5.lua and enabled ss3 correctly

--- toolchain_nao/compiler/premake5.lua
+++ toolchain_nao/compiler/premake5.lua
@@ -3,7 +3,7 @@ local linux = "i686-berlinunited-linux-gnu"
 local gcc = "4.9.3" 
 
 -- intel atom
-local cpu_flags = "-m32 -march=i686 -msse -msse2 -mssse3"
+local cpu_flags = "-m32 -march=i686 -msse -msse2 -msse3 -mssse3"
 
 -- handy definitions for some pathes
 local crossDir = path.getabsolute(".") --NAO_CROSSCOMPILE .. "/" .. linux
@@ -21,30 +21,43 @@ local cross_flags =
    
 -- the following steps are needed to add the nao cross compiler to the platforms
 -- extend the command line option list
-table.insert(premake.option.list["platform"].allowed, { "Nao", "Nao v4 (from NaoTH)" })
+--table.insert(premake.option.list["platform"].allowed, { "Nao", "Nao v5 (from NaoTH)" })
 -- extend the global variable
+--[[
 premake.platforms.Nao = 
 { 
   cfgsuffix       = "Nao",
   iscrosscompiler = true,
 }
+--]]
 -- and one more (we are called after the automatic binding)
-table.insert(premake.fields.platforms.allowed, "Nao")
+-- table.insert(premake.fields.platforms.allowed, "Nao")
 
 -- GCC/G++ settings
-premake.gcc.platforms.Nao =
+--[[
+premake.tools.gcc.platforms.Nao =
 {
   --
   cppflags = "-MMD",
   flags = cpu_flags .. cross_flags
 }
+]]--
+
+local gcc = premake.tools.gcc
 
 if(_OPTIONS["platform"] == "Nao") then
+  buildoptions {"-MMD"} -- its standard anyway
+  buildoptions {cpu_flags .. cross_flags}
   -- reset compiler path to the cross compiler
-  premake.gcc.cc     = crossDir .. "/bin/" .. linux .. "-gcc"
-  premake.gcc.cxx    = crossDir .. "/bin/" .. linux .. "-g++"
-  premake.gcc.ar     = crossDir .. "/bin/" .. linux .. "-ar"
+  gcc.tools.cc     = crossDir .. "/bin/" .. linux .. "-gcc"
+  gcc.tools.cxx    = crossDir .. "/bin/" .. linux .. "-g++"
+  gcc.tools.ar     = crossDir .. "/bin/" .. linux .. "-ar"
   print("INFO: GCC path was changed for cross compiling to")
   print("> " .. crossDir)
+end
+
+-- this seems to be necessary in order to have the compiler paths set in the generated Makefile
+function gcc.gettoolname(cfg, tool)
+   return gcc.tools[tool]
 end
 

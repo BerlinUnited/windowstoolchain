bf5161c85b3e7e0a5f66c669b2176a1bb46bf105
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2020-03-02 14:57:51 +0100

    support for usage of both, gcc and clang for cross compillation

--- toolchain_nao/compiler/premake5.lua
+++ toolchain_nao/compiler/premake5.lua
@@ -1,36 +1,71 @@
 --
 local linux = "i686-berlinunited-linux-gnu"
-local gcc = "4.9.3" 
+local version = "4.9.3" 
 
 -- intel atom
-local cpu_flags = "-m32 -march=i686 -msse -msse2 -msse3 -mssse3"
+local cpu_flags = "-m32 -march=atom -mmmx -msse -msse2 -msse3 -mssse3"
 
 -- handy definitions for some pathes
 local crossDir = path.getabsolute(".") --NAO_CROSSCOMPILE .. "/" .. linux
 local crossSystemDir = crossDir .. "/" .. linux
 
--- system pathes needed for cross compilation
-local cross_flags = 
-   " --sysroot=" .. crossSystemDir .. "/sysroot/" ..
-   " -isystem"   .. crossSystemDir .. "/sysroot/usr/include/" .. 
-   " -isystem"   .. crossSystemDir .. "/include/c++/" .. gcc .. "/" .. 
-   " -L"         .. crossSystemDir .. "/sysroot/usr/lib/"
-
 local gcc = premake.tools.gcc
 
 if(_OPTIONS["platform"] == "Nao") then
-  buildoptions {"-MMD"} -- its standard anyway
-  buildoptions {cpu_flags .. cross_flags}
-  -- reset compiler path to the cross compiler
-  gcc.tools.cc     = crossDir .. "/bin/" .. linux .. "-gcc"
-  gcc.tools.cxx    = crossDir .. "/bin/" .. linux .. "-g++"
-  gcc.tools.ar     = crossDir .. "/bin/" .. linux .. "-ar"
-  print("INFO: GCC path was changed for cross compiling to")
-  print("> " .. crossDir)
+  
+  if _OPTIONS["clang"] ~= nil then
+    print("INFO: Crosscompile with CLANG")
+    toolset "clang"
+    
+    -- speciffic options for clang
+    buildoptions {
+      "-target i686-pc-linux-gnu"
+    }
+    linkoptions {
+      "-target i686-pc-linux-gnu",
+      "-fuse-ld=" .. COMPILER_PATH_NAO .. "/bin/i686-berlinunited-linux-gnu-ld.exe"
+    }
+  else
+    print("INFO: Crosscompile with GCC " .. version)
+    
+    -- reset compiler path to the cross compiler
+    gcc.tools.cc     = crossDir .. "/bin/" .. linux .. "-gcc"
+    gcc.tools.cxx    = crossDir .. "/bin/" .. linux .. "-g++"
+    gcc.tools.ar     = crossDir .. "/bin/" .. linux .. "-ar"
+    
+    print("INFO: GCC path was changed for cross compiling to")
+    print("> " .. crossDir)
+  end
+  
+  buildoptions {
+    "-MMD", -- its standard anyway
+    cpu_flags,
+    "--sysroot=" .. crossSystemDir .. "/sysroot/"
+  }
+  sysincludedirs {
+    crossSystemDir .. "/sysroot/usr/include/",
+    crossSystemDir .. "/include/c++/" .. version .. "/",
+    crossSystemDir .. "/include/c++/" .. version .. "/" .. linux
+  }
+  linkoptions {
+      "--sysroot=" .. crossSystemDir .. "/sysroot/", 
+      "-v"
+  }
+  syslibdirs {
+    "\"" .. crossDir .. "/lib/gcc/" .. linux .. "/" .. version .. "/" .. "\"",
+    crossSystemDir .. "/sysroot/lib/",
+    crossSystemDir .. "/sysroot/usr/lib/"
+  }
 end
 
 -- this seems to be necessary in order to have the compiler paths set in the generated Makefile
 function gcc.gettoolname(cfg, tool)
-   return gcc.tools[tool]
+  return gcc.tools[tool]
 end
+
+newoption {
+   trigger     = "clang",
+   description = "set the compiler to be clang"
+}
+
 
